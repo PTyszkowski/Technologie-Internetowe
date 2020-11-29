71a086e8eeecda0116f0a26086c731a7f1b6334c
PTyszkowski <piotr.tyszkowski99@gmail.com>
2020-11-29 11:23:11 +0100

    Zad1.2e,Zad1.3; saved to csv

--- Lista7.py
+++ Lista7.py
@@ -1,12 +1,24 @@
 import pyodbc
 
+f = open("Zad1.csv", "w")
+
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=DESKTOP-835DRDL;' #nazwa serwer (komputera)
                       'Database=AdventureWorksTI2014;'
                       'Trusted_Connection=yes;')
 
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM AdventureWorksTI2014.Sales.SalesTerritory')
+cursor.execute('SELECT OH.CustomerID, SalesPersonID, StoreID,OH.TerritoryID, OH.AccountNumber FROM AdventureWorksTI2014.Sales.SalesOrderHeader OH JOIN AdventureWorksTI2014.Sales.Customer C ON OH.CustomerID = C.CustomerID')
 
+f.write('CustomerID,SalesPersonID,StoreID,TerritoryID,AccountNumber\n')
 for row in cursor:
-    print(row)
+    for i in str(row)[1:-1]:
+        if i == ' ':
+            continue
+        if i == "'":
+            continue
+        else:
+            f.write(i)
+    f.write("\n")
+
+f.close()
