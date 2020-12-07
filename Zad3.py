10d456a4edd53644e494c362d452b9e8bab5cb15
PTyszkowski <piotr.tyszkowski99@gmail.com>
2020-12-07 21:22:36 +0100

    Zad3_4

--- Zad3.py
+++ Zad3.py
@@ -1,6 +1,7 @@
 import requests as rq
 import json
 from datetime import date, timedelta
+import matplotlib.pyplot as plt
 
 def API_call(api):
 
@@ -8,7 +9,7 @@ def API_call(api):
     text = json.dumps(api,sort_keys=(True), indent = 4)
     x = json.loads(text)
     return x
-def latest_rate(currency, days):
+def latest_rate_average(currency, days):
     today =  str(date.today())
     days_ago = str(date.today() - timedelta(days=days))
     api = 'https://api.exchangeratesapi.io/history?start_at=' + days_ago + '&end_at=' + today + '&base=PLN'
@@ -18,4 +19,26 @@ def latest_rate(currency, days):
         sum += data['rates'][rating_date][currency]
     return sum/len(data['rates'])
 
-print(latest_rate('EUR',5))
+def latest_rate_plot(currency, days):
+    today =  str(date.today())
+    days_ago = str(date.today() - timedelta(days=days))
+    api = 'https://api.exchangeratesapi.io/history?start_at=' + days_ago + '&end_at=' + today + '&base=PLN'
+    data = API_call(api)
+    rates = []
+    for rating_date in data['rates']:
+        rates.append(data['rates'][rating_date][currency])
+    plt.plot(rates)
+    plt.legend([currency, "rate"])
+
+
+
+print(latest_rate_average('EUR',365/2))
+print(latest_rate_average('USD',365/2))
+latest_rate_plot('EUR',365/2)
+latest_rate_plot('USD',365/2)
+
+plt.title("USD and EUR rates")
+plt.xlabel("time (days)")
+plt.ylabel("rate (base PLN)")
+plt.show()
+plt.savefig("USD_and_EUR_rates.eps")
