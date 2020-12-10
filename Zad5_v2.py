aedf672c5d1ba697cb6a56452f73c7db764947f2
PTyszkowski <piotr.tyszkowski99@gmail.com>
2020-12-10 15:06:04 +0100

    zad5_v2 working

--- Zad5_v2.py
+++ Zad5_v2.py
@@ -1,6 +1,7 @@
 import requests as rq
 import json
 from datetime import date, timedelta
+import pyodbc
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=DESKTOP-835DRDL;' #nazwa serwer (komputera)
@@ -39,11 +40,29 @@ dates.append(today)
 print(len(list(EUR_rate.keys())))
 print(len(dates))
 
-last_rate = API_call('https://api.exchangeratesapi.io/' + str(today - timedelta(days=i)) + '?base=USD')
+last_rate = API_call('https://api.exchangeratesapi.io/' + str(today - timedelta(days=i)) + '?base=USD')['rates']['EUR']
+up_or_down = {}
+
 for i in dates:
     if i not in list(EUR_rate.keys()):
         EUR_rate[i] = last_rate
+    if EUR_rate[i] > last_rate:
+        up_or_down[i] = -1
+    elif EUR_rate[i] < last_rate:
+        up_or_down[i] = 1
+    else:
+        up_or_down[i] = 0
     last_rate = EUR_rate[i]
 
 print(len(list(EUR_rate.keys())))
 print(len(dates))
+
+# for i in EUR_rate:
+#     print('%s, %f'%(i ,EUR_rate[i]))
+
+id = 0
+for i in EUR_rate:
+    cursor.execute("INSERT INTO Currency VALUES ('%d','PLN','USD','%f','%s')"%(id,EUR_rate[i],i))
+    id += 1
+#conn.commit()
+print(up_or_down)
